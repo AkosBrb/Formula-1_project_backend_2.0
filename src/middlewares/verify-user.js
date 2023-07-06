8f4af4fe130e57972e8792ff9f90fc3bb5910b11
akos borbely <macos.borbely@gmail.com>
2023-07-06 15:33:34 +0200

    init verify user middleware

---
+++ src/middlewares/verify-user.js
@@ -0,0 +1,20 @@
+import usersModel from "../user/users-model";
+
+async function verifyUser(req, res, next) {
+    try {
+        const userId = req.params;
+        const userEmail = await usersModel.read(req.user.email)
+        const { id: authUserId } = req.user;
+
+        if (!userId && !userEmail) return next(console.log("User is missing!"))
+        if (!authUserId) return next(console.log("Token is missing!"))
+
+        if (userId === authUserId || userEmail) return next()
+
+        return next(console.log("Permission denied!"))
+    } catch (err) {
+        next(err)
+    }
+}
+
+export default verifyUser

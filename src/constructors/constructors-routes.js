f5450354bec4c668ef1943048772749f875648c4
akos borbely <macos.borbely@gmail.com>
2023-07-18 15:32:45 +0200

    constructor/:id route remove

--- src/constructors/constructors-routes.js
+++ src/constructors/constructors-routes.js
@@ -4,7 +4,6 @@ import constructorController from "./constructors-controller";
 const constructorsRouter = Router();
 
 constructorsRouter.get('/all', constructorController.readAll);
-constructorsRouter.get('/constructor/:id', constructorController.read);
 constructorsRouter.post('/add', constructorController.add);
 constructorsRouter.patch('/update/:id', constructorController.update);
 constructorsRouter.delete('/delete/:id', constructorController.delete);

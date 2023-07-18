7d4562cc91409c13cb9edc7648be19c6a0d8f510
akos borbely <macos.borbely@gmail.com>
2023-07-18 17:59:10 +0200

    verify implement

--- src/results/results-router.js
+++ src/results/results-router.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import resultsController from "./results-controller";
+import verifyUser from "../middlewares/verify-user";
+import verifyAdmin from "../middlewares/verify-admin";
 
 const resultsRouter = Router();
 
 resultsRouter.get('/', resultsController.readAll);
-resultsRouter.post('/', resultsController.add);
+resultsRouter.post('/', verifyUser, verifyAdmin, resultsController.add);
 
 export default resultsRouter

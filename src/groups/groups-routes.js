cb293cccdda0e463b2035e8b48205203b32df1ac
akos borbely <macos.borbely@gmail.com>
2023-07-18 17:59:01 +0200

    verify implement

--- src/groups/groups-routes.js
+++ src/groups/groups-routes.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import groupsController from "./groups-controller";
+import verifyUser from "../middlewares/verify-user";
 
 const groupsRouter = Router();
 
 groupsRouter.get('/', groupsController.readAll);
-groupsRouter.patch('/:id', groupsController.update);
-groupsRouter.post('/', groupsController.add);
-groupsRouter.delete('/:id', groupsController.delete);
+groupsRouter.patch('/:id', verifyUser, groupsController.update);
+groupsRouter.post('/', verifyUser, groupsController.add);
+groupsRouter.delete('/:id', verifyUser, groupsController.delete);
 groupsRouter.get('/members/:id', groupsController.listAllMember);
-groupsRouter.post('/add-member', groupsController.addMember);
-groupsRouter.delete('/delete-member', groupsController.deleteMember);
+groupsRouter.post('/add-member', verifyUser, groupsController.addMember);
+groupsRouter.delete('/delete-member', verifyUser, groupsController.deleteMember);
 
 export default groupsRouter;

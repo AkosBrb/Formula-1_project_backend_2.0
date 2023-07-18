bd85508aedec744595fe8e453c54070074326208
akos borbely <macos.borbely@gmail.com>
2023-07-18 17:58:40 +0200

    verify implement

--- src/constructors/constructors-routes.js
+++ src/constructors/constructors-routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import constructorController from "./constructors-controller";
+import verifyAdmin from "../middlewares/verify-admin";
+import verifyUser from "../middlewares/verify-user";
 
 const constructorsRouter = Router();
 
-constructorsRouter.get('/all', constructorController.readAll);
-constructorsRouter.post('/add', constructorController.add);
-constructorsRouter.patch('/update/:id', constructorController.update);
-constructorsRouter.delete('/delete/:id', constructorController.delete);
+constructorsRouter.get('/all', verifyUser, constructorController.readAll);
+constructorsRouter.post('/add', verifyUser, verifyAdmin, constructorController.add);
+constructorsRouter.patch('/update/:id', verifyUser, verifyAdmin, constructorController.update);
+constructorsRouter.delete('/delete/:id', verifyUser, verifyAdmin, constructorController.delete);
 
 export default constructorsRouter

8f5c46347a75542302386c7f0a2899d41f9c6e17
akos borbely <macos.borbely@gmail.com>
2023-07-18 17:59:18 +0200

    verify implement

--- src/tipps/tipps-router.js
+++ src/tipps/tipps-router.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import tippsController from "./tipps-controller";
+import verifyUser from "../middlewares/verify-user";
+import verifyAdmin from "../middlewares/verify-admin";
 
 const tippsRouter = Router();
 
-tippsRouter.get('/:search', tippsController.read);
-tippsRouter.post('/', tippsController.add);
-tippsRouter.delete('/:raceWeekend');
+tippsRouter.get('/:search', verifyUser, tippsController.read);
+tippsRouter.post('/', verifyUser, tippsController.add);
+tippsRouter.delete('/:raceWeekend', verifyUser, verifyAdmin, tippsController.delete);
 
 export default tippsRouter

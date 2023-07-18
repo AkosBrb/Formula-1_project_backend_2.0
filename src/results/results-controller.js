f13cf09ff1e0955a5c0b485654b21446f89f7138
akos borbely <macos.borbely@gmail.com>
2023-07-18 17:32:54 +0200

    results controller init

---
+++ src/results/results-controller.js
@@ -0,0 +1,23 @@
+import resultsService from "./results-service"
+
+const resultsController = {
+    readAll: async (req, res, next) => {
+        try {
+            const results = await resultsService.readAllResult(req.query);
+            res.json(results)
+        } catch (err) {
+            next(err)
+        }
+    },
+
+    add: async (req, res, next) => {
+        try {
+            const newResult = await resultsService.addResult(req.body);
+            res.json(newResult)
+        } catch (err) {
+            next(err)
+        }
+    }
+}
+
+export default resultsController

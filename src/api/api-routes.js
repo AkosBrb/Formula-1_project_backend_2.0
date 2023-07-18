35790642c5aac83319410f670b6535ae305767fe
akos borbely <macos.borbely@gmail.com>
2023-07-18 15:31:25 +0200

    tipps route added

--- src/api/api-routes.js
+++ src/api/api-routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import racersRouter from "../racers/racers-routes";
 import constructorsRouter from "../constructors/constructors-routes";
 import groupsRouter from "../groups/groups-routes";
+import tippsRouter from "../tipps/tipps-router";
 
 const apiRouter = Router();
 
 apiRouter.use('/racers', racersRouter)
 apiRouter.use('/constructors', constructorsRouter)
 apiRouter.use('/groups', groupsRouter)
+apiRouter.use('/tipps', tippsRouter)
 
 export default apiRouter

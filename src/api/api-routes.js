30f6560d1f8719f5b0cdbafc95440a73519a7762
akos borbely <macos.borbely@gmail.com>
2023-07-24 14:29:55 +0200

    init users route

--- src/api/api-routes.js
+++ src/api/api-routes.js
@@ -4,6 +4,7 @@ import constructorsRouter from "../constructors/constructors-routes";
 import groupsRouter from "../groups/groups-routes";
 import tippsRouter from "../tipps/tipps-router";
 import resultsRouter from "../results/results-router";
+import usersRouter from "../user/users-router";
 
 const apiRouter = Router();
 
@@ -12,5 +13,6 @@ apiRouter.use('/constructors', constructorsRouter)
 apiRouter.use('/groups', groupsRouter)
 apiRouter.use('/tipps', tippsRouter)
 apiRouter.use('/results', resultsRouter)
+apiRouter.use('/users', usersRouter)
 
 export default apiRouter

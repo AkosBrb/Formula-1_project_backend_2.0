e8c79ca858821f6b9dccc760e4c0aa89a33378e5
akos borbely <macos.borbely@gmail.com>
2023-07-24 14:32:33 +0200

    racers routes changed

--- src/racers/racers-routes.js
+++ src/racers/racers-routes.js
@@ -5,10 +5,9 @@ import verifyAdmin from "../middlewares/verify-admin";
 
 const racersRouter = Router();
 
-racersRouter.get('/all', racersController.readAll)
-racersRouter.delete('/delete/:id', verifyUser, verifyAdmin, racersController.delete)
-racersRouter.get('/racer/:id', racersController.read)
-racersRouter.post('/add', verifyUser, verifyAdmin, racersController.add)
-racersRouter.patch('/update/:id', verifyUser, verifyAdmin, racersController.update)
+racersRouter.get('/:name', racersController.readAll)
+racersRouter.delete('/:id', verifyUser, verifyAdmin, racersController.delete)
+racersRouter.post('/', verifyUser, verifyAdmin, racersController.add)
+racersRouter.patch('/:id', verifyUser, verifyAdmin, racersController.update)
 
 export default racersRouter

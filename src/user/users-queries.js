d52f0f12be9b17ddacc8378da3d5dedd493136b3
akos borbely <macos.borbely@gmail.com>
2023-07-24 14:33:39 +0200

    users queries refactoring

--- src/user/users-queries.js
+++ src/user/users-queries.js
@@ -1,14 +1,13 @@
 export const createUserQuery = `
-    INSERT INTO users (id, email, password, name, birth) VALUES (
-        $1, $2, $3, $4, $5
-    )
+INSERT INTO users (id, email, password, name, birth)
+VALUES ($1, $2, $3, $4, $5)
 `;
+
 export const readUsersQuery = `
-    SELECT * FROM users
+SELECT * FROM users
+WHERE ($1::varchar IS NULL OR $1 = email)
 `;
-export const readUserQuery = `
-    SELECT * FROM users WHERE email = $1
-`;
+
 export const updateUser = `
     UPDATE users SET email = $1, name = $2, birth = $3 WHERE id = $4
 `;

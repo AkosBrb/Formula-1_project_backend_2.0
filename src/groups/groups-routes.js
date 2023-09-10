9ee663e7638408726375a2de37ae3b33ec954eeb
akos borbely <macos.borbely@gmail.com>
2023-09-10 15:21:12 +0200

    verify changed authorize

--- src/groups/groups-routes.js
+++ src/groups/groups-routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import groupsController from "./groups-controller";
 import verifyUser from "../middlewares/verify-user";
+import authorize from '../middlewares/authorize'
 
 const groupsRouter = Router();
 
 groupsRouter.get('/', groupsController.readAll);
 groupsRouter.patch('/:id', verifyUser, groupsController.update);
-groupsRouter.post('/', verifyUser, groupsController.add);
+groupsRouter.post('/', authorize, groupsController.add);
 groupsRouter.delete('/:id', verifyUser, groupsController.delete);
 groupsRouter.get('/members/:groupId', groupsController.listAllMember);
 groupsRouter.post('/add-member/:groupId', verifyUser, groupsController.addMember);

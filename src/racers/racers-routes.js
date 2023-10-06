35c9307b8ac30b71f3e547b2850e9a72f7de454b
akos borbely <macos.borbely@gmail.com>
2023-10-06 09:33:37 +0200

    get route changed

--- src/racers/racers-routes.js
+++ src/racers/racers-routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import racersController from "./racers-controller";
+import authorize from "../middlewares/authorize";
 import verifyUser from "../middlewares/verify-user";
 import verifyAdmin from "../middlewares/verify-admin";
 
 const racersRouter = Router();
 
-racersRouter.get('/:name', racersController.readAll)
+racersRouter.get('/', authorize, verifyUser, racersController.readAll)
 racersRouter.delete('/:id', verifyUser, verifyAdmin, racersController.delete)
 racersRouter.post('/', verifyUser, verifyAdmin, racersController.add)
 racersRouter.patch('/:id', verifyUser, verifyAdmin, racersController.update)

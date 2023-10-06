47a7c0635639cca35127a8c9043479d742f07cdd
akos borbely <macos.borbely@gmail.com>
2023-10-06 08:28:51 +0200

    add authorize

--- src/tipps/tipps-router.js
+++ src/tipps/tipps-router.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import tippsController from "./tipps-controller";
 import verifyUser from "../middlewares/verify-user";
 import verifyAdmin from "../middlewares/verify-admin";
+import authorize from "../middlewares/authorize";
 
 const tippsRouter = Router();
 
-tippsRouter.get('/:search', verifyUser, tippsController.read);
-tippsRouter.post('/', verifyUser, tippsController.add);
-tippsRouter.delete('/:raceWeekend', verifyUser, verifyAdmin, tippsController.delete);
+tippsRouter.get('/:search', authorize, verifyUser, tippsController.read);
+tippsRouter.post('/', authorize, verifyUser, tippsController.add);
+tippsRouter.delete('/:raceWeekend', authorize, verifyUser, verifyAdmin, tippsController.delete);
 
 export default tippsRouter
